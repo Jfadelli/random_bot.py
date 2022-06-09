265cf8f0f9d0c09c31f75a3c4bb4838394aa048b
Jason Fadelli <jfadelli@gmail.com>
2022-06-09 13:49:52 -0700

    test v1.1

--- test1.py
+++ test1.py
@@ -1,11 +1,10 @@
 import random
 
-
 command_words = [".decide", ".d", ".roll", ".r"]
 
 def find_command(message):
     for word in message.split(" "):
-        print(f'command word: {word}')
+        # print(f'command word: {word}')
         if word in command_words:
             return [message.find(word), word]
         else:
@@ -17,22 +16,25 @@ def roll_parser(message):
 
     if dice_value[0] == "d":
         dice_value = dice_value[1::]
-    print(random.randint(1,int(dice_value)))
+    print(f'console: {random.randint(1,int(dice_value))}')
         
-
 def message_parser(message):
     curr_command = find_command(message)[1]
-    print(f'line 13 -> Current Command : {curr_command}')
+
     if curr_command == "no command":
+        print(f'console: {curr_command}')
         pass
         
     elif curr_command == '.d' or curr_command == '.decide':
         curr_list = message.split(", ")
         curr_list[0] = curr_list[0][((len(curr_command))+1)::]
+        print(f'console: {random.choice(curr_list)}')
         return curr_list
+
     elif curr_command == '.r' or curr_command == '.roll':
         roll_parser(message)
+
     else:
         return "no command"
 
-message_parser('.r d220')
+message_parser(input())
